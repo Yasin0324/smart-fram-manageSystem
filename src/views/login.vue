f2fa47273544cf6c2e300910dee286cbb761a9a3
Yasin0324 <119337341+Yasin0324@users.noreply.github.com>
2024-03-05 20:19:35 +0800

    feat:登录页面

---
+++ src/views/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login">
+    <el-form>
+      <h3>智慧农场后台管理系统</h3>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style scoped lang="scss">
+.login{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
